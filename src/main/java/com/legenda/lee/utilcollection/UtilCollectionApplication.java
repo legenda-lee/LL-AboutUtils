cfd9531e5f91a7a5acabb50be85021e01be78544
Legenda-Lee <lee.legenda@gmail.com>
2019-11-16 20:55:31 +0800

    init

---
+++ src/main/java/com/legenda/lee/utilcollection/UtilCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.legenda.lee.utilcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/legenda/lee/utilcollection/UtilCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legenda.lee.utilcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
